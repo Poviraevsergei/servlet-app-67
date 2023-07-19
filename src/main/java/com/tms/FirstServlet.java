4c110a6ada6a197a245fe0f8c730c95c97f93e90
Poviraevsergei <poviraevsergei@gmail.com>
2023-07-19 20:32:09 +0300

    create servlet(annotation)

--- src/main/java/com/tms/FirstServlet.java
+++ src/main/java/com/tms/FirstServlet.java
@@ -1,6 +1,7 @@
 package com.tms;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
