ce0b7949cc8d29cab5e8fbe541877570d983257e
Poviraevsergei <poviraevsergei@gmail.com>
2023-07-24 19:41:57 +0300

    servlet filter

--- src/main/java/com/tms/FirstServlet.java
+++ src/main/java/com/tms/FirstServlet.java
@@ -1,6 +1,9 @@
 package com.tms;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,13 +12,35 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("Hello from first servlet. Group 67!");
+        String name = req.getParameter("name");
+        writer.println("My name is " + name);
         writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Servlet started ...");
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+        System.out.println("Servlet work ...");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet destroyed ...");
     }
 }
---
+++ src/main/java/com/tms/filter/LogFilter.java
@@ -0,0 +1,19 @@
+package com.tms.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+
+public class LogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter work!");
+        chain.doFilter(request, response);
+    }
+}
