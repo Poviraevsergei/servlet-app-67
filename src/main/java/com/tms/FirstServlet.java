b6d8a60fd45c815dff62486676a0030f1dc2d323
Poviraevsergei <poviraevsergei@gmail.com>
2023-08-02 19:42:04 +0300

    JSP

--- src/main/java/com/tms/FirstServlet.java
+++ src/main/java/com/tms/FirstServlet.java
@@ -22,12 +22,9 @@ import java.io.Writer;
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String fname = req.getParameter("fname");
-        String lname = req.getParameter("lname");
-        Writer writer = resp.getWriter();
-        writer.write("First: " + fname);
-        writer.write("Last: " + lname);
-        writer.close();
+        String name = req.getParameter("name");
+        req.setAttribute("name", name);
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req,resp);
     }
 
     @Override
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Group 65</title>
+</head>
+<body>
+name: ${name}
+name: <%= request.getAttribute("name") %>
+</body>
+</html>
